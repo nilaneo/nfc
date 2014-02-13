2fcf60006030de786f965d676e244a35bc87d399
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2014-02-13 15:46:18 +0200

    Make custom placeholders

--- js/main.js
+++ js/main.js
@@ -3,4 +3,17 @@ $(function() {
 		e.preventDefault();
 		$(this).toggleClass("checked");
 	});
+
+	/*
+	* SIMPLE PLACEHOLDERS
+	*/
+	$("input[type=text]").focus(function(){
+		var t = $(this)
+		if( t.val()==t.attr('def') ) t.val('')
+	});
+
+		$("input[type=text]").blur(function(){
+		var t = $(this)
+		if( t.val().length==0 ) t.val( t.attr('def') )
+	});
 });
